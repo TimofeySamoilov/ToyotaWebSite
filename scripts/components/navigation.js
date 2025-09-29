6b121ecc89d438a567c5af7d556dc4f74e4cc4dd
Bevilion 19 <tssamoilov@edu.hse.com>
2025-09-29 21:11:48 +0300

    initial commit

---
+++ scripts/components/navigation.js
@@ -0,0 +1,13 @@
+
+async function loadNavigation() {
+    try {
+        const response = await fetch('/navigation.html');
+        const html = await response.text();
+        document.getElementById('navbar-placeholder').innerHTML = html;
+    } catch (error) {
+        console.log('Navigation loading failed', error)
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', loadNavigation)
